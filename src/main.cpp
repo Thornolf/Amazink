ba56dd2d0297b0f9b548a5eaac04802f2b3b7157
Quentin Baudet <quentin.baudet@epitech.eu>
2019-01-31 23:51:09 -0500

    fix: modify the array

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,11 @@ int main(int argc, char **argv)
 {
   (void)argc;
   (void)argv;
-  std::cout << "Salut c'est moi" << std::endl;
+  std::string* ptr = new std::string[argc];
+
+  for (int index = 1 ; index < argc; index++) {
+    ptr[index] = argv[index];
+    std::cout << "L'attribu numéro " << index << " du tableau est "  << ptr[index] << std::endl;
+  }
   return (0);
 }
