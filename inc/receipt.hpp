c0cfcd8b084fea55b759c959e939c36624e4d3a3
Quentin Baudet <quentin.baudet@epitech.eu>
2019-02-06 16:54:14 -0500

    fix: prompt

--- inc/receipt.hpp
+++ inc/receipt.hpp
@@ -8,22 +8,15 @@ class		Receipt{
 private:
   int		_total;
   Cart		*_cart;
-  std::string	_print;
-  std::string	_fileName;
 public:
   Receipt();
   Receipt(int, Cart*);
   int		getTotal() const;
   Cart		*getCart() const;
-  std::string	getPrint() const;
-  std::string	getFileName() const;
   void		setTotal(int);
   void		setCart(Cart*);
-  void		setPrint(std::string);
-  void		setFileName(std::string);
   void		calculingTotalAmount(const Cart &);
   void		display();
-  void		printing();
   virtual	~Receipt();
 };
 
--- src/receipt.cpp
+++ src/receipt.cpp
@@ -1,7 +1,6 @@
 #include	"receipt.hpp"
 
 #include	<fstream>
-#include	<ctime>
 
 Receipt::Receipt() {
 }
@@ -18,13 +17,6 @@ Cart		*Receipt::getCart() const {
   return (this->_cart);
 }
 
-std::string	Receipt::getPrint() const {
-  return (this->_print);
-}
-
-std::string	Receipt::getFileName() const {
-  return (this->_fileName);
-}
 void		Receipt::setTotal(int newTotal) {
   this->_total = newTotal;
 }
@@ -33,13 +25,6 @@ void		Receipt::setCart(Cart *newCart) {
   this->_cart = newCart;
 }
 
-void		Receipt::setPrint(std::string newPrint) {
-  this->_print = newPrint;
-}
-
-void		Receipt::setFileName(std::string newFileName) {
-  this->_fileName = newFileName;
-}
 void		Receipt::display() {
   std::cout << "\nYour ordered has been placed." << std::endl;
   std::cout << "You bought these items : " << std::endl;
@@ -50,40 +35,6 @@ void		Receipt::display() {
   }
   std::cout << "---------------------------------\n" << std::endl;
   std::cout << "The amount is " << this->_total << " $\n" << std::endl;
-  printing();
-  std::cout << "Your receipt was printed in the file " << this->_fileName << std::endl;
 }
 
-void		Receipt::printing() {
-  this->_fileName = "receipt-";
-  this->_print = "Your ordered has been placed\n";
-  this->_print += "You bought these items : \n";
-  this->_print += "---------------------------------\n";
-  for (int i = 0; i < this->_cart->getSize(); i++) {
-    this-> _print +=  "[";
-    this->_print += std::to_string(i);
-    this->_print += "] ";
-    this->_print += this->_cart->getItems()[i].getName();
-    this->_print += " | ";
-    this->_print += this->_cart->getItems()[i].getRGB();
-    this->_print += " | ";
-    this->_print += std::to_string(this->_cart->getItems()[i].getPrice());
-    this->_print += " $\n";
-  }
-  this->_print += "\n---------------------------------\n\n";
-  this->_print +="The amout is : ";
-  this->_print += std::to_string(this->_total);
-  this->_print += " $\n";
-  this->_print += "\nThank you for purchasing on Amazink !\nHave a nice day !\n";
-  std::time_t result = std::time(nullptr);
-  std::asctime(std::localtime(&result));
-  this->_fileName += std::to_string(result);
-  this->_fileName += ".txt";
-  std::string logDirectory = "log/";
-  logDirectory += this->_fileName;
-  this->_fileName = logDirectory;
-  std::ofstream outfile (this->_fileName);
-  outfile << this->_print << std::endl;
-  outfile.close();
-}
 Receipt::~Receipt() {}
